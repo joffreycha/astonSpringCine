d7a0b47aaff8962afda34a006c6703081299f073
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-04-01 11:15:16 +0200

    Add model Cinema

---
+++ src/main/java/com/aston/models/Cinema.java
@@ -0,0 +1,21 @@
+package com.aston.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cinema {
+
+	@Id
+	private String id;
+	
+	private String nom;
+	private String adresse;
+}
