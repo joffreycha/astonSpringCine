cbeafde3defeafb11892b211176ddd5be1f6f6d2
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-04-01 11:15:01 +0200

    Add model Salle

---
+++ src/main/java/com/aston/models/Salle.java
@@ -0,0 +1,26 @@
+package com.aston.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Salle {
+
+	@Id
+	private String id;
+	
+	private String nom;
+	
+	@DBRef
+	private Cinema cinema;
+
+	private int place;
+}
