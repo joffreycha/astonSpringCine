f2bc06485b4901b2770a2851f103ad3f29953894
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-04-01 11:11:30 +0200

    Add model Client

---
+++ src/main/java/com/aston/models/Client.java
@@ -0,0 +1,25 @@
+package com.aston.models;
+
+import java.time.LocalDate;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+	
+	@Id
+	private String id;
+	
+	private String nom;
+	private boolean etudient;
+	private LocalDate naissance;
+
+}
