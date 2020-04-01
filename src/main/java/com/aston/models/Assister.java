2faa104ab12e52069d68f0c251d980139cbede21
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-04-01 11:15:32 +0200

    Add model Assister

---
+++ src/main/java/com/aston/models/Assister.java
@@ -0,0 +1,20 @@
+package com.aston.models;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Assister {
+
+	private float prix;
+	
+	@DBRef
+	private Client client;
+}
