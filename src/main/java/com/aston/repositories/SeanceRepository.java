1b40f477fd89acfa830bd0fce2f757bedae60899
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-04-01 11:15:56 +0200

    Add SeanceRepository

---
+++ src/main/java/com/aston/repositories/SeanceRepository.java
@@ -0,0 +1,9 @@
+package com.aston.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.aston.models.Seance;
+
+public interface SeanceRepository extends MongoRepository<Seance, String> {
+
+}
