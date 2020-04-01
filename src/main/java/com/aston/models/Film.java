24f0bd9cb06db9a87d8970e8c1f28e349c5cb613
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-04-01 10:32:10 +0200

    Add model Film

---
+++ src/main/java/com/aston/models/Film.java
@@ -0,0 +1,24 @@
+package com.aston.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Film {
+
+	@Id
+	private String id;
+	
+	private String titre;
+	private int duree; // in minutes
+	private int ageLimite;
+	private String genre;
+	private String visa;
+}
